552b4e43c9dce229c627c4a5073b955e19c4ecf5
Angry Coding <mailidanielifechukwu@gmail.com>
2022-08-14 20:06:34 +0100

    hardhat simpleStorage

---
+++ scripts/deploy.js
@@ -0,0 +1,10 @@
+const hre = require("hardhat");
+
+async function main() {}
+
+main()
+    .then(() => process.exit(1))
+    .catch((error) => {
+        console.log(error);
+        process.exit(0);
+    });
