329a77883a7b08b7d52b87bd7844c6ff3f5a9f19
Angry Coding <mailidanielifechukwu@gmail.com>
2022-08-22 10:42:36 +0100

    added ether scan verfication

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 module.exports = {
     defaultNetwork: "rinkeby",
     networks: {
@@ -14,4 +16,9 @@ module.exports = {
         },
     },
     solidity: "0.8.9",
+    etherscan: {
+        apiKey: {
+            rinkeby: ETHERSCAN_API_KEY,
+        },
+    },
 };
